04546eefce896de72a260e14e9508282a6909513
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 17:37:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assing({},playlist,{[artistName]:songTitle})
+  return Object.assign({},playlist,{[artistName]:songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName) {
